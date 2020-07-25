3b8730c3a4bc5336e12d8c9f036c9f553fb3eaca
cajy78 <cajy78@live.com>
2020-07-25 21:23:02 +0530

    added additional type casts

--- learnCoreJava/src/basicOperations/typeCastExample.java
+++ learnCoreJava/src/basicOperations/typeCastExample.java
@@ -11,6 +11,25 @@ public class typeCastExample {
 		float conv = Float.parseFloat(val);
 		System.out.println("The value entered is: "+conv);
 		s1.close();
+		
+		//implicit type cast.
+		int i = 10;
+		long l = i;
+		float f = l;
+		System.out.println("Example of int: "+i);
+		System.out.println("Example of int to long: "+l);
+		System.out.println("Example of long: "+f);
+		
+		//explicit type casting
+		double d = 100.100;
+		f = (float)d;
+		l = (long)f;
+		i = (int)l;
+		
+		System.out.println("Example of double: "+d);
+		System.out.println("Example of double to float (Explicit caste): "+f);
+		System.out.println("Example of float to long (explicit): "+l);
+		System.out.println("Example of long to int (explicit): "+i);
 
 	}
 
